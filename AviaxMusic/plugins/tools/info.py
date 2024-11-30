f96fde6d8b1b9e06067ed39beb74b5ed6216ff05
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-11-30 10:05:59 +0530

    Update info.py

--- AviaxMusic/plugins/tools/info.py
+++ AviaxMusic/plugins/tools/info.py
@@ -1,132 +1,127 @@
-from time import ctime
+"""
+import os
 
 from pyrogram import filters
-from pyrogram.types import (InlineQuery, InlineQueryResultArticle,
-                            InputTextMessageContent, Message)
+from pyrogram.types import Message
 
 from AviaxMusic.misc import SUDOERS
-from AviaxMusic.utils.db import (add_chat, add_user, chat_exists,
-                          get_blacklist_event, get_nsfw_count,
-                          get_reputation, get_user_trust,
-                          is_chat_blacklisted, is_user_blacklisted,
-                          user_exists)
+from AviaxMusic.core.sections import section
+from AviaxMusic.utils.dbfunctions import is_gbanned_user, user_global_karma
 
 __MODULE__ = "Info"
 __HELP__ = """
-**Get Info About A Chat Or User**
-
-/info [CHAT_ID/Username|USER_ID/Username]
-
-or you can use inline mode >>
-@SpamProtectionRobot [CHAT_ID/Username|USER_ID/Username]
+/info [USERNAME|ID] - Get info about a user.
+/chat_info [USERNAME|ID] - Get info about a chat.
 """
 
 
-async def get_user_info(user):
-    try:
-        user = await spr.get_users(user)
-    except Exception:
-        return
-    if not user_exists(user.id):
-        add_user(user.id)
-    trust = get_user_trust(user.id)
-    blacklisted = is_user_blacklisted(user.id)
-    reason = None
-    if blacklisted:
-        reason, time = get_blacklist_event(user.id)
-    data = f"""
-**ID:** {user.id}
-**DC:** {user.dc_id}
-**Username:** {user.username}
-**Mention: ** {user.mention("Link")}
-
-**Is Sudo:** {user.id in SUDOERS}
-**Trust:** {trust}
-**Spammer:** {True if trust < 50 else False}
-**Reputation:** {get_reputation(user.id)}
-**NSFW Count:** {get_nsfw_count(user.id)}
-**Potential Spammer:** {True if trust < 70 else False}
-**Blacklisted:** {blacklisted}
-"""
-    data += (
-        f"**Blacklist Reason:** {reason} | {ctime(time)}"
-        if reason
-        else ""
-    )
-    return data
+async def get_user_info(user, already=False):
+    if not already:
+        user = await app.get_users(user)
+    if not user.first_name:
+        return ["Deleted account", None]
+    user_id = user.id
+    username = user.username
+    first_name = user.first_name
+    mention = user.mention("Link")
+    dc_id = user.dc_id
+    photo_id = user.photo.big_file_id if user.photo else None
+    is_gbanned = await is_gbanned_user(user_id)
+    is_sudo = user_id in SUDOERS
+    is_premium = user.is_premium
+    karma = await user_global_karma(user_id)
+    body = {
+        "ID": user_id,
+        "DC": dc_id,
+        "Name": [first_name],
+        "Username": [("@" + username) if username else "Null"],
+        "Mention": [mention],
+        "Sudo": is_sudo,
+        "Premium": is_premium,
+        "Karma": karma,
+        "Gbanned": is_gbanned,
+    }
+    caption = section("User info", body)
+    return [caption, photo_id]
 
 
-async def get_chat_info(chat):
-    try:
-        chat = await spr.get_chat(chat)
-    except Exception:
-        return
-    if not chat_exists(chat.id):
-        add_chat(chat.id)
-    blacklisted = is_chat_blacklisted(chat.id)
-    reason = None
-    if blacklisted:
-        reason, time = get_blacklist_event(chat.id)
-    data = f"""
-**ID:** {chat.id}
-**Username:** {chat.username}
-**Type:** {chat.type}
-**Members:** {chat.members_count}
-**Scam:** {chat.is_scam}
-**Restricted:** {chat.is_restricted}
-**Blacklisted:** {blacklisted}
-"""
-    data += (
-        f"**Blacklist Reason:** {reason} | {ctime(time)}"
-        if reason
-        else ""
-    )
-    return data
+async def get_chat_info(chat, already=False):
+    if not already:
+        chat = await app.get_chat(chat)
+    chat_id = chat.id
+    username = chat.username
+    title = chat.title
+    type_ = str(chat.type).split(".")[1]
+    is_scam = chat.is_scam
+    description = chat.description
+    members = chat.members_count
+    is_restricted = chat.is_restricted
+    link = f"[Link](t.me/{username})" if username else "Null"
+    dc_id = chat.dc_id
+    photo_id = chat.photo.big_file_id if chat.photo else None
+    body = {
+        "ID": chat_id,
+        "DC": dc_id,
+        "Type": type_,
+        "Name": [title],
+        "Username": [("@" + username) if username else "Null"],
+        "Mention": [link],
+        "Members": members,
+        "Scam": is_scam,
+        "Restricted": is_restricted,
+        "Description": [description],
+    }
+    caption = section("Chat info", body)
+    return [caption, photo_id]
 
 
-async def get_info(entity):
-    user = await get_user_info(entity)
-    if user:
-        return user
-    chat = await get_chat_info(entity)
-    return chat
-
-
-@spr.on_message(filters.command("info"), group=3)
+@app.on_message(filters.command("info"))
 async def info_func(_, message: Message):
     if message.reply_to_message:
-        reply = message.reply_to_message
-        user = reply.from_user
-        entity = user.id or message.chat.id
-    elif len(message.command) == 1:
-        user = message.from_user
-        entity = user.id or message.chat.id
-    elif len(message.command) == 2:
-        entity = message.text.split(None, 1)[1]
-    else:
-        return await message.reply_text("Read the help menu")
-    entity = await get_info(entity)
-    entity = entity or "I haven't seen this chat/user."
-    await message.reply_text(entity)
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+
+    m = await message.reply_text("Processing")
+
+    try:
+        info_caption, photo_id = await get_user_info(user)
+    except Exception as e:
+        return await m.edit(f"{str(e)}, Perhaps you meant to use /chat_info ?")
+
+    if not photo_id:
+        return await m.edit(info_caption, disable_web_page_preview=True)
+    photo = await app.download_media(photo_id)
+
+    await message.reply_photo(photo, caption=info_caption, quote=False)
+    await m.delete()
+    os.remove(photo)
 
 
-@spr.on_inline_query()
-async def inline_info_func(_, query: InlineQuery):
-    query_ = query.query.strip()
-    entity = await get_info(query_)
-    if not entity:
-        err = "I haven't seen this user/chat."
-        results = [
-            InlineQueryResultArticle(
-                err,
-                input_message_content=InputTextMessageContent(err),
+@app.on_message(filters.command("chat_info"))
+async def chat_info_func(_, message: Message):
+    splited = message.text.split()
+    if len(splited) == 1:
+        chat = message.chat.id
+        if chat == message.from_user.id:
+            return await message.reply_text(
+                "**Usage:**/chat_info [USERNAME|ID]"
             )
-        ]
     else:
-        results = [
-            InlineQueryResultArticle(
-                "Found Entity",
-                input_message_content=InputTextMessageContent(entity),
-            )
-        ]
-    await query.answer(results=results, cache_time=3)
+        chat = splited[1]
+    try:
+        m = await message.reply_text("Processing")
+
+        info_caption, photo_id = await get_chat_info(chat)
+        if not photo_id:
+            return await m.edit(info_caption, disable_web_page_preview=True)
+
+        photo = await app.download_media(photo_id)
+        await message.reply_photo(photo, caption=info_caption, quote=False)
+
+        await m.delete()
+        os.remove(photo)
+    except Exception as e:
+        await m.edit(e)
