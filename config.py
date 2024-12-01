eec1185b14c403c81e782b5045b639fef8b37f1f
[🇮🇳]˼ Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ ˹♡︎ـﮩ🫧 <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-12-01 09:45:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LucyXDbot/zxc_mera_beta",
+    "https://github.com/MAHTO-ANJALI/Vm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/where_lucy")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/mystic_legion")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ANJALIWORLD")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ANJALINETWORK")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
